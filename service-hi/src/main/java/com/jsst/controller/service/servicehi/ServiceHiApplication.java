83b31c36a035a98a9b6b26f537278c1a14c67e58
WangGJ <guangjian.wang@jieshunpay.cn>
2018-07-27 14:14:30 +0800

    config add Zipkin in service-hi  And service-miya

--- service-hi/src/main/java/com/jsst/controller/service/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/jsst/controller/service/servicehi/ServiceHiApplication.java
@@ -1,12 +1,19 @@
 package com.jsst.controller.service.servicehi;
 
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,11 +25,41 @@ public class ServiceHiApplication {
         SpringApplication.run( ServiceHiApplication.class, args );
     }
 
-    @Value("${server.port}")
-    String port;
+//    @Value("${server.port}")
+//    String port;
+
+//    @RequestMapping("/hi")
+//    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+//        return "hi " + name + " ,i am from port:" + port;
+//    }
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8764/miya", String.class);
     }
+
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+
+
 }
---
+++ service-miya/src/main/java/com/jsst/servicemiya/ServiceMiyaApplication.java
@@ -0,0 +1,52 @@
+package com.jsst.servicemiya;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceMiyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMiyaApplication.class, args);
+    }
+   private static final Logger LOG = Logger.getLogger(ServiceMiyaApplication.class.getName());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hi")
+    public String home(){
+        LOG.log(Level.INFO, "hi is being called");
+        return "hi i'm miya!";
+    }
+
+    @RequestMapping("/miya")
+    public String info(){
+        LOG.log(Level.INFO, "info is being called");
+        return restTemplate.getForObject("http://localhost:8763/info",String.class);
+    }
+
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
