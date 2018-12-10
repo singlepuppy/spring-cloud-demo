9f3db136af7b9783876f16b2a22470f765c162f6
WangGJ <guangjian.wang@jieshunpay.cn>
2018-12-10 16:16:54 +0800

    乱七八糟

--- service-hi/src/main/java/com/jsst/controller/service/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/jsst/controller/service/servicehi/ServiceHiApplication.java
@@ -2,6 +2,7 @@ package com.jsst.controller.service.servicehi;
 
 import brave.sampler.Sampler;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,11 +29,14 @@ import java.util.logging.Logger;
 @EnableHystrix  //开启熔断器
 @EnableHystrixDashboard //开启熔断监控
 @EnableCircuitBreaker
+@Log4j2
 public class ServiceHiApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run( ServiceHiApplication.class, args );
+        System.out.println("ServiceHiApplication.main");
+        log.info("啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
     }
 /*********************************服务注册+熔断例子***********************/
 //    @Value("${server.port}")
@@ -42,32 +47,32 @@ public class ServiceHiApplication {
 //        return "hi " + name + " ,i am from port:" + port;
 //    }
 /***********************************zipkin例子************************************************/
-//    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    @Bean
-//    public RestTemplate getRestTemplate(){
-//        return new RestTemplate();
-//    }
-//
-//    @RequestMapping("/hi")
-//    public String callHome(){
-//        LOG.log(Level.INFO, "calling trace service-hi  ");
-//        return restTemplate.getForObject("http://localhost:8764/miya", String.class);
-//    }
-//
-//    @RequestMapping("/info")
-//    public String info(){
-//        LOG.log(Level.INFO, "calling trace service-hi ");
-//        return "i'm service-hi";
-//
-//    }
-//
-//    @Bean
-//    public Sampler defaultSampler() {
-//        return Sampler.ALWAYS_SAMPLE;
-//    }
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping(method = RequestMethod.GET ,value = "hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8764/miya", String.class);
+    }
+
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 /*******************************熔断监控例子******************************************/
     /**
      * 访问地址 http://localhost:8763/actuator/hystrix.stream
@@ -76,7 +81,7 @@ public class ServiceHiApplication {
     @Value("${server.port}")
     String port;
 
-    @RequestMapping("/hi")
+    @RequestMapping("/hello")
     @HystrixCommand(fallbackMethod = "hiError")   //断路点声明
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
--- service-miya/src/main/java/com/jsst/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/jsst/servicemiya/ServiceMiyaApplication.java
@@ -1,10 +1,15 @@
 package com.jsst.servicemiya;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +20,11 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix  //开启熔断器
+@EnableHystrixDashboard //开启熔断监控
+@EnableCircuitBreaker
 public class ServiceMiyaApplication {
 
     public static void main(String[] args) {
@@ -27,9 +36,14 @@ public class ServiceMiyaApplication {
     private RestTemplate restTemplate;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "miyaError")   //断路点声明
     public String home(){
         LOG.log(Level.INFO, "hi is being called");
         return "hi i'm miya!";
+    }
+
+    public String miyaError(String name) {
+        return "miya,sorry,error!";
     }
 
     @RequestMapping("/miya")
